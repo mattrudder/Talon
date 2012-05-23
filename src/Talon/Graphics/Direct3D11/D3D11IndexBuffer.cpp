c734a4aa0cc73d3790c4b660260d2e7a213cb813
Matt Rudder <matt@mattrudder.com>
2012-05-22 21:30:24 -0700

    Catching up D3D11 Index and Vertex Buffer up with OpenGL.

--- src/Talon/Graphics/Direct3D11/D3D11IndexBuffer.cpp
+++ src/Talon/Graphics/Direct3D11/D3D11IndexBuffer.cpp
@@ -1,6 +1,7 @@
 
 #include "TalonPrefix.h"
 #include <Talon/Graphics/IndexBuffer.h>
+#include <Talon/Graphics/RenderDevice.h>
 
 using namespace std;
 
@@ -9,15 +10,32 @@ namespace Talon
     class IndexBuffer::Impl
     {
     public:
+		CComPtr<ID3D11Buffer> indexBuffer;
     };
 
-    IndexBuffer::IndexBuffer(RenderDevice* renderDevice, int indexCount, BufferFormat format, void* /*initialData*/, BufferUsage bufferUsage)
+    IndexBuffer::IndexBuffer(RenderDevice* renderDevice, int indexCount, BufferFormat format, void* initialData, BufferUsage bufferUsage)
         : m_renderDevice(renderDevice)
         , m_format(format)
         , m_indexCount(indexCount)
         , m_bufferUsage(bufferUsage)
         , m_pImpl(make_unique<Impl>())
     {
+		size_t indexSize = format == BufferFormat::I16 ? sizeof(i16) : sizeof(i32);
+
+		D3D11_BUFFER_DESC bufferDesc = {0};
+		bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		bufferDesc.ByteWidth = indexSize * indexCount;
+		bufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		bufferDesc.CPUAccessFlags = 0;
+		bufferDesc.MiscFlags = 0;
+		bufferDesc.StructureByteStride = 0;
+
+		D3D11_SUBRESOURCE_DATA bufferData = {0};
+		bufferData.pSysMem = initialData;
+		bufferData.SysMemPitch = 0;
+		bufferData.SysMemSlicePitch = 0;
+
+		TALON_HR(renderDevice->GetDevice()->CreateBuffer(&bufferDesc, &bufferData, &m_pImpl->indexBuffer));
 	}
 
 
--- src/Talon/Graphics/Direct3D11/D3D11VertexBuffer.cpp
+++ src/Talon/Graphics/Direct3D11/D3D11VertexBuffer.cpp
@@ -1,6 +1,7 @@
 
 #include "TalonPrefix.h"
 #include <Talon/Graphics/VertexBuffer.h>
+#include <Talon/Graphics/RenderDevice.h>
 
 using namespace std;
 
@@ -9,15 +10,30 @@ namespace Talon
     class VertexBuffer::Impl
     {
     public:
+		CComPtr<ID3D11Buffer> vertexBuffer;
     };
 
-    VertexBuffer::VertexBuffer(RenderDevice* renderDevice, int vertexSize, int vertexCount, void* /*initialData*/, BufferUsage bufferUsage)
+    VertexBuffer::VertexBuffer(RenderDevice* renderDevice, int vertexSize, int vertexCount, void* initialData, BufferUsage bufferUsage)
         : m_renderDevice(renderDevice)
         , m_vertexSize(vertexSize)
         , m_vertexCount(vertexCount)
         , m_bufferUsage(bufferUsage)
         , m_pImpl(make_unique<Impl>())
     {
+		D3D11_BUFFER_DESC bufferDesc = {0};
+		bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		bufferDesc.ByteWidth = vertexSize * vertexCount;
+		bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		bufferDesc.CPUAccessFlags = 0;
+		bufferDesc.MiscFlags = 0;
+		bufferDesc.StructureByteStride = 0;
+
+		D3D11_SUBRESOURCE_DATA bufferData = {0};
+		bufferData.pSysMem = initialData;
+		bufferData.SysMemPitch = 0;
+		bufferData.SysMemSlicePitch = 0;
+
+		TALON_HR(renderDevice->GetDevice()->CreateBuffer(&bufferDesc, &bufferData, &m_pImpl->vertexBuffer));
     }
 
     VertexBuffer::~VertexBuffer()
