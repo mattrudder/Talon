830f97b27750532ef11d050494d8d66b6aa4ec2a
Matt Rudder <matt@mattrudder.com>
2012-06-19 10:24:25 -0700

    Cleaning up.

--- src/Talon/TalonPrefix.h
+++ src/Talon/TalonPrefix.h
@@ -24,9 +24,6 @@ using namespace nowide;
 #	define WIN32_LEAN_AND_MEAN // TODO: Define additional NO_* defines to slim down windows.h
 #	define PATH_MAX MAX_PATH
 #	define NAME_MAX 255
-#	include <wincodec.h>	// Windows Imaging Component (WIC)
-#	include <wincodecsdk.h>
-#	pragma comment(lib, "WindowsCodecs.lib")
 #	include <atlbase.h> // For CComPtr
 #	include <windows.h>
 #	include <Talon/Platform/Win32/ComException.h>
