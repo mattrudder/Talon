1814b512414c26a2bfbac43177e28087a054f348
Matt Rudder <matt@mattrudder.com>
2012-06-19 08:45:26 -0700

    Matrix jazz.

--- include/Talon/Math/Matrix.h
+++ include/Talon/Math/Matrix.h
@@ -12,10 +12,36 @@
 
 namespace Talon
 {
+	struct Matrix;
+
+	// TODO: Support passing by value for systems that support it (e.g, Xbox 360)
+	typedef const Matrix& MatrixArgL1;
+
 	TALON_ALIGN(16, struct Matrix
 	{
 		Vector Rows[4];
+
+		Matrix& operator= (const Matrix& m) { r[0] = m.r[0]; r[1] = m.r[1]; r[2] = m.r[2]; r[3] = m.r[3]; return *this; }
+			    
+		Matrix operator+ () const { return *this; }
+		Matrix operator- () const;
+			    
+		Matrix& operator+= (MatrixArgL1 m);
+		Matrix& operator-= (MatrixArgL1 m);
+		Matrix& operator*= (MatrixArgL1 m);
+		Matrix& operator*= (float s);
+		Matrix& operator/= (float s);
+			    
+		Matrix operator+ (MatrixArgL1 m) const;
+		Matrix operator- (MatrixArgL1 m) const;
+		Matrix operator* (MatrixArgL1 m) const;
+		Matrix operator* (float s) const;
+		Matrix operator/ (float s) const;
+
+		friend Matrix operator* (float s, MatrixArgL1 m);
 	});
+
+	// TODO: Matrix free functions
 }
 
 #include "Matrix.inl"
--- include/Talon/Math/Matrix.inl
+++ include/Talon/Math/Matrix.inl
@@ -3,4 +3,55 @@
 
 namespace Talon
 {
+	Matrix Matrix::operator- () const
+	{
+
+	}
+
+	Matrix& Matrix::operator+= (MatrixArgL1 m)
+	{
+
+	}
+	Matrix& Matrix::operator-= (MatrixArgL1 m)
+	{
+
+	}
+	Matrix& Matrix::operator*= (MatrixArgL1 m)
+	{
+
+	}
+	Matrix& Matrix::operator*= (float s)
+	{
+
+	}
+	Matrix& Matrix::operator/= (float s)
+	{
+
+	}
+
+	Matrix Matrix::operator+ (MatrixArgL1 m) const
+	{
+
+	}
+	Matrix Matrix::operator- (MatrixArgL1 m) const
+	{
+
+	}
+	Matrix Matrix::operator* (MatrixArgL1 m) const
+	{
+
+	}
+	Matrix Matrix::operator* (float s) const
+	{
+
+	}
+	Matrix Matrix::operator/ (float s) const
+	{
+
+	}
+
+	Matrix Matrix::operator* (float s, MatrixArgL1 m)
+	{
+
+	}
 }
