e3bf8d4b20538301367e17f632af00ceea71dea0
Matt Rudder <matt@mattrudder.com>
2012-07-01 13:57:16 -0700

    Making GL textures support Update.

--- src/Talon/Graphics/OpenGL/GLTexture.cpp
+++ src/Talon/Graphics/OpenGL/GLTexture.cpp
@@ -13,6 +13,7 @@ namespace Talon
         Impl();
         ~Impl();
         
+		BufferFormat format;
         GLuint texture;
         u32 width;
         u32 height;
@@ -38,15 +39,23 @@ namespace Talon
         glDeleteTextures(1, &texture);
     }
     
-	bool Texture::Load(u32 width, u32 height, BufferFormat format, const void* data, const std::string debugName)
+	bool Texture::Load(u32 width, u32 height, BufferFormat format, BufferUsage usage, const void* data, const std::string debugName)
 	{
 		glBindTexture(GL_TEXTURE_2D, m_pImpl->texture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL::ToInternalFormat(format), width, height, 0, GL::ToFormat(format), GL::ToDataType(format), data);
         glBindTexture(GL_TEXTURE_2D, 0);
         
+		m_pImpl->format = format;
 		m_pImpl->width = width;
 		m_pImpl->height = height;
 		return true;
+	}
+	
+	void Texture::Update(const void *data)
+	{
+		glBindTexture(GL_TEXTURE_2D, m_pImpl->texture);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_pImpl->width, m_pImpl->height, GL::ToFormat(m_pImpl->format), GL::ToDataType(m_pImpl->format), data);
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
     
 	u32 Texture::GetWidth() const
