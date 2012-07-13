89bcb116db3a56b9e38815b639a5c64130e2792f
Matt Rudder <matt@mattrudder.com>
2012-07-13 08:47:28 -0700

    Added GetTextFromFile implementation.

--- src/Talon/Platform/Win32/Win32Platform.cpp
+++ src/Talon/Platform/Win32/Win32Platform.cpp
@@ -7,7 +7,7 @@ namespace Talon
 {
 	bool Platform::SetWorkingPath(const std::string& path)
 	{
-		return SetCurrentDirectory(convert(path).c_str()) == TRUE;
+		return SetCurrentDirectoryW(convert(path).c_str()) == TRUE;
 	}
 
 	std::string Platform::GetEnginePath()
@@ -18,11 +18,32 @@ namespace Talon
 
 	std::string Platform::GetResourcePath(const std::string& fileName)
 	{
-		return fileName;
+		// TODO: Find the actual file name, returning empty string if not found.
+		return GetEnginePath() + fileName;
 	}
 
 	std::string Platform::GetTextFromFile(const std::string& fileName)
 	{
-		return fileName;
+		std::string fullFileName = GetResourcePath(fileName);
+		std::string contents;
+		HANDLE file = CreateFileW(convert(fullFileName).c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+		if (file != INVALID_HANDLE_VALUE)
+		{
+			DWORD bytesRead = 0;
+			DWORD fileSize = GetFileSize(file, nullptr);
+			char* fileData = new char[fileSize + 1];
+			BOOL success = ReadFile(file, fileData, fileSize, &bytesRead, nullptr);
+			if (success && bytesRead == fileSize)
+			{
+				fileData[fileSize] = '\0';
+				contents = fileData;
+			}
+
+			delete [] fileData;
+
+			CloseHandle(file);
+		}
+
+		return contents;
 	}
 }
